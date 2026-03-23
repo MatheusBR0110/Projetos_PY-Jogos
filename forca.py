2f09ea6277bd024f21bdf91a35aef8957e94cee1
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-03-23 11:34:36 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,20 +3,23 @@ def jogar_forca():
     print("Bem-Vindo ao jogo da Forca")
     print("--------------------------")
 
+    lista = []
+
     palavra_secreta = "processador"
     perdeu = False
     acertou = False
 
     while(not perdeu and not acertou):
         chute = input("Digite uma letra: ")
+        chute = chute.strip()
 
+        #index define a posição da letra
+        index = 0
 
-
-
-
-
-
-
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                print(f"A letra {chute} está na posição {index}")
+            index = index + 1
 
 
 if(__name__ == "__main__"):
