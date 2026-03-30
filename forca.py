fd8e24b4da88cb4191951fc7529980b351080780
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-03-30 11:47:24 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -3,25 +3,41 @@ def jogar_forca():
     print("Bem-Vindo ao jogo da Forca")
     print("--------------------------")
 
-    lista = []
+    arquivo = open("Jogos/palavras.txt", "r")
+    palavras = []
 
+    for linha in palavras:
+        linha = linha.strip()
+        palavras. append(linha)
+    
     palavra_secreta = "processador".upper()
-    letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_"]
+    letras_acertadas = []
+
+    for letra in palavra_secreta:
+        letras_acertadas.append()
+
     perdeu = False
     acertou = False
+    erros = 0
 
     while(not perdeu and not acertou):
         chute = input("Digite uma letra: ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         #index define a posição da letra
         index = 0
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index = index + 1       
+        else:
+            erros += 1
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
 
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-                print(letras_acertadas)
-            index = index + 1
+        print(letras_acertadas)
+        
         
 
 
